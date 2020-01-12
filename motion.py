27f2168718430d65d5849712e7c12ae1c32c4599
txlim96 <30485660+txlim96@users.noreply.github.com>
2020-01-12 19:18:42 +0800

    Add files via upload

--- motion.py
+++ motion.py
@@ -1,196 +1,132 @@
-#!/usr/bin/env python3
 import RPi.GPIO as gpio
 from time import sleep
-import os
-import subprocess
-import math
-##import NDIR
-from bme280 import readBME280All
-from cam import picam, webcam
+from math import pi
 
 # stepper motor constants
-DELAY       = 0.00005 # 5us, max 300khz
-TOTAL_STEPS = 200     # steps to reach max speed
-MICROSTEPPING = 16*200    # 1/16
+DELAY         = 0.00005
+TOTAL_STEPS   = 200
+MICROSTEPPING = 16*200
 
 # geometry constants
-RADIUS1 = 34.75  # in mm
-RADIUS2 = 12
-DISP_PER_REV1 = 0.002*math.pi*RADIUS1   # displacement per revolution in m
-DISP_PER_REV2 = 0.002*math.pi*RADIUS2
+RADIUS_FB   = 34.75
+RADIUS_TELE = 12
+DPR_FB      = 0.002*pi*RADIUS_FB
+DPR_TELE    = 0.002*pi*RADIUS_TELE
 
 # telescopic constants
-TELESCOPIC_LENGTH = 0.76
+TELE_LENGTH = 0.76
 
-step_fb = 0
+step_fb   = 0
 step_tele = 0
-fb_pins = {"dir": 0, "step": 0, "en": 0, "limit": 0}
-telescopic_pins = {
-    "dir_l": 0,
-    "step_l": 0,
-    "en_l": 0,
-    "dir_r": 0,
-    "step_r": 0,
-    "en_r": 0,
-    "limit_left": 0,
-    "limit_right": 0}
-servo_pins = {"cam1": 0, "cam1_angle": 0}
-limit_sw = 0
+PIN = {
+    "FB": {
+        "A": {
+            "DIR":  0,
+            "PUL":  0,
+            "EN":   0,
+            "LIM":  0
+            }
+        },
+    "TELESCOPE": {
+        "L": {
+            "DIR":  0,
+            "PUL":  0,
+            "EN":   0,
+            "LIM":  0
+            },
+        "R": {
+            "DIR":  0,
+            "PUL":  0,
+            "EN":   0,
+            "LIM":  0
+            }
+        }
+    }
 
 def limit(channel):
     global limit_sw
     limit_sw = channel
-    sleep(1)
 
-def pinSetup(fb, telescopic, servo):
-    global fb_pins, telescopic_pins, servo_pins
-    fb_pins["dir"] = fb["dir"]
-    fb_pins["step"] = fb["step"]
-    fb_pins["en"] = fb["en"]
-    fb_pins["limit"] = fb["limit"]
-    gpio.add_event_detect(fb_pins["limit"], gpio.FALLING, callback=limit)
-    
-    telescopic_pins["dir_l"] = telescopic["dir_l"]
-    telescopic_pins["step_l"] = telescopic["step_l"]
-    telescopic_pins["en_l"] = telescopic["en_l"]
-
-    telescopic_pins["dir_r"] = telescopic["dir_r"]
-    telescopic_pins["step_r"] = telescopic["step_r"]
-    telescopic_pins["en_r"] = telescopic["en_r"]
-
-    telescopic_pins["limit_left"] = telescopic["limit_left"]
-    telescopic_pins["limit_right"] = telescopic["limit_right"]
-    gpio.add_event_detect(telescopic_pins["limit_left"], gpio.FALLING, callback=limit)
-    gpio.add_event_detect(telescopic_pins["limit_right"], gpio.FALLING, callback=limit)
-    
-    gpio.output(fb_pins["en"], False)
-    gpio.output(telescopic_pins["en_l"], False)
-    gpio.output(telescopic_pins["en_r"], False)
-
-    servo_pins["cam1"] = servo["cam1"]
-    servo_pins["cam1_angle"] = servo["cam1_angle"]
-    
-    gpio.output(fb_pins["dir"], True)
-    gpio.output(fb_pins["en"], True)
-    if gpio.input(fb_pins["limit"]):
-        while limit_sw != fb_pins["limit"]:
-            motor(fb_pins["step"], DELAY*10)
-    gpio.output(fb_pins["en"], False)
-    print("FB")
-    
-    gpio.output(telescopic_pins["dir_l"], True)
-    gpio.output(telescopic_pins["en_l"], True)
-    if gpio.input(telescopic_pins["limit_left"]):
-        while limit_sw != telescopic_pins["limit_left"]:
-            motor(telescopic_pins["step_l"], DELAY*5)
-    gpio.output(telescopic_pins["en_l"], False)
-    print("TL")
-    
-    gpio.output(telescopic_pins["dir_r"], True)
-    gpio.output(telescopic_pins["en_r"], True)
-    if gpio.input(telescopic_pins["limit_right"]):
-        while limit_sw != telescopic_pins["limit_right"]:
-            motor(telescopic_pins["step_r"], DELAY*5)
-    gpio.output(telescopic_pins["en_r"], False)
-    print("TR")
-    
-#    servo = gpio.PWM(servo_pins["cam1"], 50)
-#    servo.start(2.5)
-#    sleep(1)
-#    servo.stop()
-
-def motor(pulsePin, duration):
-    gpio.output(pulsePin, True)
+def motor(pulse, duration):
+    gpio.output(pulse, True)
     sleep(duration)
-    gpio.output(pulsePin, False)
+    gpio.output(pulse, False)
     sleep(duration)
     return 1
+
+def calibrate(pins):
+    gpio.output(pins["DIR"], True)
+    gpio.output(pins["EN"], True)
+    if gpio.input(pins["LIM"]):
+        while limit_sw != pins["LIM"]:
+            motor(pins["PUL"], DELAY*5)
+    gpio.output(pins["EN"], False)
+
+def accelerate(pins, mode, steps_to_max=200, acc_steps=10): # steps_to_max must be a integer product of acc_steps
+    if mode == "a":
+        for repeat in range(acc_steps,0,-1):
+            for iteration in range(steps_to_max/acc_steps):
+                motor(pins, DELAY*repeat)
+        return steps_to_max
+    elif mode == "d":
+        for repeat in range(1,acc_steps,1):
+            for iteration in range(steps_to_max/acc_steps):
+                motor(pins, DELAY*repeat)
+        return steps_to_max
+
+def pinSetup(pins):
+    global PIN
+    for mode in pins:
+        for direction in pins[mode]:
+            for pinout in pins[mode][direction]:
+                PIN[mode][direction][pinout] = pins[mode][direction][pinout]
     
-def moveFrontBack(disp):
+    gpio.add_event_detect(PIN["FB"]["A"]["LIM"], gpio.FALLING, callback=limit)
+    gpio.add_event_detect(PIN["TELESCOPE"]["L"]["LIM"], gpio.FALLING, callback=limit)
+    gpio.add_event_detect(PIN["TELESCOPE"]["R"]["LIM"], gpio.FALLING, callback=limit)
+    
+    gpio.output(PIN["FB"]["A"]["EN"], False)
+    gpio.output(PIN["TELESCOPE"]["L"]["EN"], False)
+    gpio.output(PIN["TELESCOPE"]["R"]["EN"], False)
+
+#    calibrate_sequence = [PIN["TELESCOPE"]["L"], PIN["TELESCOPE"]["R"], PIN["FB"]["A"]]
+#    
+#    for sequence in calibrate_sequence:
+#        calibrate(sequence)
+
+def moveFB(disp=1.0):
     global step_fb
     step_fb = 0
-    acc = TOTAL_STEPS
-    accelerate_signal = False
-    gpio.output(fb_pins["en"], True)
-    if disp < 0:
-        print("backward")
-        gpio.output(fb_pins["dir"], True)
-    else:
-        print("forward")
-        gpio.output(fb_pins["dir"], False)
-        
-    total = abs(disp) / DISP_PER_REV1 * MICROSTEPPING
-    maxCap = total-TOTAL_STEPS
-    while step_fb <= total:
+    total = abs(disp) / DPR_FB * MICROSTEPPING
+    ul = total-TOTAL_STEPS
+    
+    gpio.output(PIN["FB"]["A"]["DIR"], disp<0)
+    gpio.output(PIN["FB"]["A"]["EN"], True)
+    while step_fb < total:
         if step_fb < TOTAL_STEPS:
-            for repeat in range(10,0,-1):
-                for iteration in range(20):
-                    step_fb += motor(fb_pins["step"], DELAY*repeat)
-        elif TOTAL_STEPS <= step_fb and step_fb < maxCap:
-            step_fb += motor(fb_pins["step"], DELAY)
+            step_fb += accelerate(PIN["FB"]["A"]["PUL"], "a")
+        elif TOTAL_STEPS <= step_fb and step_fb < ul:
+            step_fb += motor(PIN["FB"]["A"]["PUL"], DELAY)
         else:
-            for repeat in range(1,11,1):
-                for iteration in range(20):
-                    step_fb += motor(fb_pins["step"], DELAY*repeat)
-
-#        if 0 < acc and acc <= TOTAL_STEPS and not accelerate_signal:   # accelerate
-#            acc -= 20
-#            for iteration in range(50):
-#                step_fb += motor(fb_pins["step"], DELAY*acc)
-#        elif TOTAL_STEPS * 5 > total-step_fb and accelerate_signal:     # decelerate
-#            acc += 20
-#            for iteration in range(50):
-#                step_fb += motor(fb_pins["step"], DELAY*acc)
-#        else:
-#            accelerate_signal = True
-#            step_fb += motor(fb_pins["step"], DELAY)
-    gpio.output(fb_pins["en"], False)
-
-def telescopicArm():
+            step_fb += accelerate(PIN["FB"]["A"]["PUL"], "d")
+            
+    gpio.output(PIN["FB"]["A"]["EN"], False)
+    
+def moveTele():
     global step_tele
     step_tele = 0
-    servo1 = gpio.PWM(servo_pins["cam1"], 50)
-    servo1.start(2.5)
-    
     extension_sequence = [False, True]
-    sequence = ["Extend", "Retract"]
-    gpio.output(telescopic_pins["en_r"], True)
-    gpio.output(telescopic_pins["en_l"], True)
+    
+    gpio.output(PIN["TELESCOPE"]["L"]["EN"], True)
+    gpio.output(PIN["TELESCOPE"]["R"]["EN"], True)
+    
     for i in range(2):
         step_tele = 0
-        gpio.output(telescopic_pins["dir_r"], extension_sequence[i])
-        gpio.output(telescopic_pins["dir_l"], extension_sequence[i])
-        while step_tele <= TELESCOPIC_LENGTH / DISP_PER_REV2 * MICROSTEPPING:
-            step_tele += motor(telescopic_pins["step_r"], DELAY)
-            motor(telescopic_pins["step_l"], DELAY)
-        print(sequence[i])
-        if i == 0:
-            os.system("killall motion")
-            subprocess.call(["/home/pi/webcam/webcam.sh"])
-            sleep(1)
-#        if i == 0:
-#            temperature,pressure,humidity = readBME280All()
-#            file = open("/home/pi/Desktop/Nelson/sensor data.txt", "a")
-#            file.write("Temp:{0}\nPressure:{1}\nHumidity:{2}\n".format(temperature, pressure, humidity))
-#            file.close()
-#            servo1.ChangeDutyCycle(servo_pins["cam1_angle"]/18 + 2.5)
-#            sleep(1)
-##            print("Temperature:{0}\nPressure:{1}\nHumidity:{2}".format(temperature, pressure, humidity))
-#            picam()
-#            servo1.ChangeDutyCycle(2.5)
-#            sleep(1)
-#            servo1.stop()
-#            del servo1
-    gpio.output(telescopic_pins["en_r"], False)
-    gpio.output(telescopic_pins["en_l"], False)
+        gpio.output(PIN["TELESCOPE"]["L"]["DIR"], extension_sequence[i])
+        gpio.output(PIN["TELESCOPE"]["R"]["DIR"], extension_sequence[i])
+        while step_tele <= TELE_LENGTH / DPR_TELE * MICROSTEPPING:
+            step_tele += motor(PIN["TELESCOPE"]["L"]["PUL"], DELAY)
+            motor(PIN["TELESCOPE"]["R"]["PUL"], DELAY)
     
-def servoManual(angle):
-    servo = gpio.PWM(servo_pins["cam1"], 50)
-    servo.start(0)
-    d = angle/18 + 2.5
-    servo.ChangeDutyCycle(d)
-    sleep(1)
-    servo.ChangeDutyCycle(2.5)
-    sleep(1)
-    servo.stop()
-    del servo
+    gpio.output(PIN["TELESCOPE"]["L"]["EN"], False)
+    gpio.output(PIN["TELESCOPE"]["R"]["EN"], False)
